95c32aab694b1dfde98bf7e78e64bddbb70569ec
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-06-19 11:24:46 -0600

    libros2.4

--- mi-app/app/page.tsx
+++ mi-app/app/page.tsx
@@ -7,7 +7,7 @@ import FeaturedCharacters from "@/components/home/FeaturedCharacters";
 function Divider() {
   return (
     <div className="relative w-full flex justify-center">
-      <div className="w-full max-w-7xl px-6">
+      <div className="w-full max-w-15xl px-6">
         <div className="h-px bg-gradient-to-r from-transparent via-cyan-400/70 to-transparent" />
       </div>
     </div>
