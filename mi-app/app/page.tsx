d7c7c8105d1eddbabb7ae8524c65a8960c251c30
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-06-19 17:52:58 -0600

    libros6.4

--- mi-app/app/page.tsx
+++ mi-app/app/page.tsx
@@ -11,7 +11,7 @@ function Divider({ label }: { label?: string }) {
         <div className="h-[2px] flex-1 bg-gradient-to-r from-transparent via-cyan-400 to-transparent" />
 
         {label && (
-          <span className="text-[16px] md:text-xl tracking-[0.4em] text-cyan-500 uppercase whitespace-nowrap">
+          <span className="text-[16px] md:text-xl tracking-[0.3em] text-cyan-500 uppercase whitespace-nowrap">
             {label}
           </span>
         )}
