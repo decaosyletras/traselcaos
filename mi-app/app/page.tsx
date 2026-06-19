f911177cdfd45af1a8c524ddaa5308dcbdc4b3c0
programacion3 <141765274+programacion3@users.noreply.github.com>
2026-06-19 11:14:11 -0600

    libros2.1

--- mi-app/app/page.tsx
+++ mi-app/app/page.tsx
@@ -4,17 +4,27 @@ import UniversePreview from "@/components/home/UniversePreview";
 import TimelinePreview from "@/components/home/TimelinePreview";
 import FeaturedCharacters from "@/components/home/FeaturedCharacters";
 
+function Divider() {
+  return (
+    <div className="relative w-full flex justify-center">
+      <div className="w-full max-w-7xl px-6">
+        <div className="h-px bg-gradient-to-r from-transparent via-cyan-500/40 to-transparent" />
+      </div>
+    </div>
+  );
+}
+
 export default function HomePage() {
   return (
     <main>
       <Hero />
-
+      <Divider />
       <FeaturedBooks />
-
+      <Divider />
       <UniversePreview />
-
+      <Divider />
       <TimelinePreview />
-
+      {/*<Divider />*/}
       {/*<FeaturedCharacters />*/}
     </main>
   );
